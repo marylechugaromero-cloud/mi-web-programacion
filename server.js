646fc2a3d7eb7a17949fb3d57cf72f8430a3cb64
Mary Lechuga <marylechugaromero@gmail.com>
2026-05-02 18:32:53 -0600

    cambio smtp a puerto 587

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
 // 📧 CONFIG CORREO
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
