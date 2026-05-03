679ccd8332695a3b35749292dd99367fb0899428
Mary Lechuga <marylechugaromero@gmail.com>
2026-05-02 18:22:52 -0600

    cambio configuracion smtp gmail

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
 // Configuración correo
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // SSL
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
