c2397b6e461d46d8b8f96fe9c1b982e47010f021
Mary Lechuga <marylechugaromero@gmail.com>
2026-05-02 18:51:51 -0600

    cambio smtp a puerto 465

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ mongoose.connect(process.env.MONGO_URI, {
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
