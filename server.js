bf35e9d147ac073b6aa86d8ecdd023c9ab2266a4
Mary Lechuga <marylechugaromero@gmail.com>
2026-05-02 18:12:40 -0600

    fix smtp gmail timeout

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Configuración correo
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
